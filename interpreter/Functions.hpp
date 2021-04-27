4ed90b0272eac0475b85706ac9d06133e48cd5cd
antistv <piotr.b.furman@gmail.com>
2021-04-27 11:52:35 +0200

    Multi find value

--- interpreter/Functions.hpp
+++ interpreter/Functions.hpp
@@ -4,11 +4,17 @@
 #include "../parser/FunctionDefinition.hpp"
 #include <iostream>
 #include <map>
+#include <variant>
  
 namespace interpreter {
     using namespace std;
     using namespace parser;
-
+/*
+    template <typename T, std::enable_if_t<std::is_same_v<T, double> || std::is_same_v<T, std::string>, bool> = true>
+    struct findVarStruct {
+        T data;
+    };
+*/
     class Scope {
         public:
             map<string, double> doubleVarTab;
@@ -29,9 +35,9 @@ namespace interpreter {
             void changeVarValue(Statement &operations, Scope &scope);
             bool startIf(Statement &operations, Scope &scope);
         private:
-            double startCalculations(Statement &operations, Scope &scope);
-            double calculating(Statement &operations, Scope &scope);
+            variant<double, string, int, unsigned int, char, unsigned char> startCalculations(Statement &operations, Scope &scope);
+            variant<double, string, int, unsigned int, char, unsigned char> calculating(Statement &operations, Scope &scope);
             bool calculateIf(Statement &operations, Scope &scope);
-            double findVar(Statement &operations, Scope &scope);
+            variant<double, string, int, unsigned int, char, unsigned char> findVar(Statement &operations, Scope &scope);
     };
 }
